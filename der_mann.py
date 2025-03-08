57ebd9560e900fb4b0399e072c189146c21a2d60
Makazis <labdiennabagi@gmail.com>
2025-03-08 23:09:13 +0200

    added several minor features implemented a very basic level of drunkeness

---
+++ der_mann.py
@@ -0,0 +1,9 @@
+from random import *
+from math import *
+import pygame
+
+class Player:
+    def __init__(self):
+        #set to 1 for testing purpouses
+        self.driving_disability=1
+        self.temporal_perception=1
